06b845026941cb3f897bf9a9c32d561b459d6ce5
Sai Krishna <saiaws2612@gmail.com>
2025-03-06 15:06:18 +0530

    Update test.sh

--- day-4-horizonatl-scaling/test.sh
+++ day-4-horizonatl-scaling/test.sh
@@ -1,5 +1,5 @@
     for i in {1..1000}; do  
-        curl -s -o /dev/null -w "%{http_code}\n" http://a48d1d51f2945469da6ffc19238aa8ad-1307616002.ap-south-1.elb.amazonaws.com/ &  
+        curl -s -o /dev/null -w "%{http_code}\n" <loadbalancer of the pod>/ &  
     done  
     wait  # Wait for all background curl processes to finish before next iteration
 done
